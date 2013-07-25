ea20c5a93e7b129904b4e7458e4ed461a75b9fae
Christian Delahousse <christian@delahousse.ca>
2013-07-24 21:06:44 -0400

    tabs-> spaces

--- skimr.js
+++ skimr.js
@@ -2,123 +2,123 @@
 'use strict';
 
 // Google object
-var	google,
+var google,
 
 //Created document and UI elements
-		google_tag,
-		css_tag,
-		skimr_div,//container div
-		loading_div,
-		list_table,
-		dashboard_div,
-		exit_anchor,
-		next_anchor,
-		prev_anchor,
+    google_tag,
+    css_tag,
+    skimr_div,//container div
+    loading_div,
+    list_table,
+    dashboard_div,
+    exit_anchor,
+    next_anchor,
+    prev_anchor,
 
 //Private Properties
-		entries_per_page = 50,  //Entries per page
-		num_max_entries = 250, //Google feed API maxes out at 250
-		rss_url,
-		rss_urls = {},
-		current_offset = 0,
-		current_results = null;
+    entries_per_page = 50,  //Entries per page
+    num_max_entries = 250, //Google feed API maxes out at 250
+    rss_url,
+    rss_urls = {},
+    current_offset = 0,
+    current_results = null;
 
 
 //Initilisation
 function init () {
 
-	google_tag = buildGoogleTag();
-	document.getElementsByTagName('head')[0].appendChild( google_tag );
-	
-	//Check to see if the google tag is loaded
-	//and pass loadFeedAPI
-	assetReady(google_tag, function () {
+  google_tag = buildGoogleTag();
+  document.getElementsByTagName('head')[0].appendChild( google_tag );
 
-			//Move google loader object into local scope
-			google = window.google;
+  //Check to see if the google tag is loaded
+  //and pass loadFeedAPI
+  assetReady(google_tag, function () {
 
-			google.load('feeds', '1', {
-				"callback": setUpFeedURL, //Once the feed api is loaded, it calls initFeed() 
-				"nocss": true
-				});
+      //Move google loader object into local scope
+      google = window.google;
 
-		});
+      google.load('feeds', '1', {
+        "callback": setUpFeedURL, //Once the feed api is loaded, it calls initFeed() 
+        "nocss": true
+        });
 
-	ui.buildUi();
+    });
 
-	//Set events
-	eventDelegation();
+  ui.buildUi();
+
+  //Set events
+  eventDelegation();
 }
 //Try to find RSS URL
 function setUpFeedURL() {
-	rss_url = getRSSLink(); //Find RSS URL
-	
-	//run googleFeedLookup if no <link> rss exists
-	
-	rss_url ? initFeed() : googleFeedLookup(window.location,initFeed);
+  rss_url = getRSSLink(); //Find RSS URL
+
+  //run googleFeedLookup if no <link> rss exists
+
+  rss_url ? initFeed() : googleFeedLookup(window.location,initFeed);
 
 }
 // (re)initalise the feed using google feed api
 function initFeed (num,callback) {
-	var google_feed; //Google Feed object
-	
-	//Bugfix: for some odd reason, in firefox, Google obj returns 3 as an argument
-	num === 3 && (num = undefined);
+  var google_feed; //Google Feed object
 
-	//Number of entries to request
-	//Default to default entries per page if none specified, the number used for first page
-	num || (num = entries_per_page); //Faster http://jsperf.com/conditional-assignment 
+  //Bugfix: for some odd reason, in firefox, Google obj returns 3 as an argument
+  num === 3 && (num = undefined);
 
-	//Callback to be called once feed has been received by GoogFeedAPI
-	//Default to postFeedInit
-	callback || (callback = postFeedInit);
+  //Number of entries to request
+  //Default to default entries per page if none specified, the number used for first page
+  num || (num = entries_per_page); //Faster http://jsperf.com/conditional-assignment 
 
-	//if getRSSLink fails, exit app with warning to the user
-	if (!rss_url) {
-		alert("Skimr can't work on this page");
-		exitApp();
-		return;
-	}
+  //Callback to be called once feed has been received by GoogFeedAPI
+  //Default to postFeedInit
+  callback || (callback = postFeedInit);
 
-	//TODO: make warning be sliding box. Unobtrusive warning;
+  //if getRSSLink fails, exit app with warning to the user
+  if (!rss_url) {
+    alert("Skimr can't work on this page");
+    exitApp();
+    return;
+  }
 
-	google_feed = new google.feeds.Feed(rss_url);
-	
-	//Initialise feed settings
-	google_feed.setResultFormat(google.feeds.Feed.JSON_FORMAT);
-	google_feed.includeHistoricalEntries();
-	google_feed.setNumEntries(num);
-	google_feed.load(callback); //Default callback: postFeedInit
+  //TODO: make warning be sliding box. Unobtrusive warning;
+
+  google_feed = new google.feeds.Feed(rss_url);
+
+  //Initialise feed settings
+  google_feed.setResultFormat(google.feeds.Feed.JSON_FORMAT);
+  google_feed.includeHistoricalEntries();
+  google_feed.setNumEntries(num);
+  google_feed.load(callback); //Default callback: postFeedInit
 }
 
 //What to do once feed has been initialised
 //var results is passed for the google feed api (see initFeed)
 function postFeedInit (results) {
-	var entries;
-	
+  var entries;
 
-	//TODO WE NEED TO IMPLEMENT PROPER ERROR HANDLING
-	//TODO If we do implement error handling, move to program code, not methods. I think...
-	// If feed doesn't load or doesn't exist, exit app
-	if (results.status.code === 400){
-		alert('Woops, there is a problem with the feed'); 
-		exitApp();
-		//throw 'Feed 404';// TODO MUST CATCH
-	}
-	
-	//Update skimr class properties
-	current_results = results.feed.entries; 
+
+  //TODO WE NEED TO IMPLEMENT PROPER ERROR HANDLING
+  //TODO If we do implement error handling, move to program code, not methods. I think...
+  // If feed doesn't load or doesn't exist, exit app
+  if (results.status.code === 400){
+    alert('Woops, there is a problem with the feed'); 
+    exitApp();
+    //throw 'Feed 404';// TODO MUST CATCH
+  }
+
+  //Update skimr class properties
+  current_results = results.feed.entries; 
 
   //TODO THIS IS WHERE NEW RESULTS SHOULD BE PASSED IN
   ui.updateUi( results.feed.entries );
 
-	//Preload remaining for pagination
-	initFeed(num_max_entries,function (){
-		current_results = this.feed.entries;
+  //Preload remaining for pagination
+  initFeed(num_max_entries,function (){
+    current_results = this.feed.entries;
     //XXX SHOULD BE THIS ui.updateUi( results.feed.entries );
 
-		//Allow Pagination via next button
-		// TODO Refactor this out
+    //Allow Pagination via next button
+    // TODO Refactor this out
     current_results.length >= entries_per_page && (next_anchor.className = 'show');
   });
 }
@@ -126,101 +126,101 @@ function postFeedInit (results) {
 //Attaches events to UI
 function eventDelegation () {
 
-	var doc = document;
-	if (doc.addEventListener){ 
-		doc.addEventListener("keydown", keyEvents, false);
-		doc.addEventListener("click", clickEvents, false);
-	}
+  var doc = document;
+  if (doc.addEventListener){ 
+    doc.addEventListener("keydown", keyEvents, false);
+    doc.addEventListener("click", clickEvents, false);
+  }
 
-	//Keyboard Events	
-	function keyEvents (event) {
-		event || (event = window.event); //Handle IE window.event. Ew...
+  //Keyboard Events 
+  function keyEvents (event) {
+    event || (event = window.event); //Handle IE window.event. Ew...
 
 
-		switch (event.keyCode) {
-			//Escape key
-			case 27:
-				exitApp();
-				break;
+    switch (event.keyCode) {
+      //Escape key
+      case 27:
+        exitApp();
+        break;
 
-			//Left arrow
-			case 37:
-				//don't go back when on first listings
-				current_offset > 0 && ui.pagination(-entries_per_page);
-				break;
+      //Left arrow
+      case 37:
+        //don't go back when on first listings
+        current_offset > 0 && ui.pagination(-entries_per_page);
+        break;
 
-			//Right
-			case 39:
-				//don't go forward when none lie beyond current page listings
-				entries_per_page >= (current_results.length - current_offset) || ui.pagination(entries_per_page);
-				break;
-		}
-	}
+      //Right
+      case 39:
+        //don't go forward when none lie beyond current page listings
+        entries_per_page >= (current_results.length - current_offset) || ui.pagination(entries_per_page);
+        break;
+    }
+  }
 
-	//Click Events
-	function clickEvents (event){
-		var target = event.target;
+  //Click Events
+  function clickEvents (event){
+    var target = event.target;
 
-		switch (target.id) {
-			case 'skimr-exit':
-			exitApp();
-			break;
-			case 'skimr-next':
-			ui.pagination(entries_per_page);
-			break;
-			case 'skimr-prev':
-			ui.pagination(-entries_per_page);
-			break;
-		}
-	}
+    switch (target.id) {
+      case 'skimr-exit':
+      exitApp();
+      break;
+      case 'skimr-next':
+      ui.pagination(entries_per_page);
+      break;
+      case 'skimr-prev':
+      ui.pagination(-entries_per_page);
+      break;
+    }
+  }
 }
 
 //Scans the <link> tags. Searches for type - application/rss+xml and returns
 //the hypertext reference. If none, returns false
 function getRSSLink () {
-	var	link_nodes = document.getElementsByTagName('link'),
-			location = window.location, //local version of location object
-			rss_link = false, //false until proven found. Initialised as not found
-			n = link_nodes.length,
-			current_node,
-			node_type;
+  var link_nodes = document.getElementsByTagName('link'),
+      location = window.location, //local version of location object
+      rss_link = false, //false until proven found. Initialised as not found
+      n = link_nodes.length,
+      current_node,
+      node_type;
 
-	for (var i = 0; i < n; i++) {
+  for (var i = 0; i < n; i++) {
 
-		current_node = link_nodes[i];
-		node_type = current_node.getAttribute('type');
+    current_node = link_nodes[i];
+    node_type = current_node.getAttribute('type');
 
-		//Get first RSS or Atom match
-		if (node_type == 'application/rss+xml' || node_type == 'application/atom+xml') {
+    //Get first RSS or Atom match
+    if (node_type == 'application/rss+xml' || node_type == 'application/atom+xml') {
 
-			rss_link = current_node.getAttribute('href');
+      rss_link = current_node.getAttribute('href');
 
-			//If it's not a full on URL, we need to convert it to one
-			if (!rss_link.match(/^http/)) {
-				var absolute_path = (rss_link.match(/^\//)) ? '': location.pathname;
-				rss_link = location.protocol + '//' + location.hostname + absolute_path + rss_link;
-			}
-			//If we've found a link, we don't need to continue looping
-			break;
-		}
-	}
-	return rss_link;
+      //If it's not a full on URL, we need to convert it to one
+      if (!rss_link.match(/^http/)) {
+        var absolute_path = (rss_link.match(/^\//)) ? '': location.pathname;
+        rss_link = location.protocol + '//' + location.hostname + absolute_path + rss_link;
+      }
+      //If we've found a link, we don't need to continue looping
+      break;
+    }
+  }
+  return rss_link;
 }
 
 //Use feed api to find feed URL
 function googleFeedLookup (url,callback){
-		google.feeds.lookupFeed(url,function(results){
-			rss_url = results.url;
-			console.log('Google Feed Lookup returned: ',results.url)
-			callback();
-	});
+    google.feeds.lookupFeed(url,function(results){
+      rss_url = results.url;
+      console.log('Google Feed Lookup returned: ',results.url)
+      callback();
+  });
 }
 
 //builds that tag that will load the google api
 function buildGoogleTag (){ 
-	var google_tag = document.createElement('script');
-	google_tag.src = 'https://www.google.com/jsapi';
-	return google_tag;
+  var google_tag = document.createElement('script');
+  google_tag.src = 'https://www.google.com/jsapi';
+  return google_tag;
 }
 
 
@@ -228,33 +228,33 @@ function exitApp () {
 
   ui.deleteUi();
 
-	//Delete global object;
-	window.skimr && (delete window.skimr);
-	
+  //Delete global object;
+  window.skimr && (delete window.skimr);
+
   skimr_script && skimr_script.parentNode.removeChild( skimr_script );
-	//Delete script tag created by outside run script
-	window.skimr_script && (delete window.skimr_script); 
-	//For outside run script test (window.skmir.exitApp)
-	return true;
+  //Delete script tag created by outside run script
+  window.skimr_script && (delete window.skimr_script); 
+  //For outside run script test (window.skmir.exitApp)
+  return true;
 
 }
 
 //Run fn when given asset is  loaded
 function assetReady(asset,fn) {
-	//the Google JS API is weird and takes a while to load,
-	//so we need to be sure to load it completely before we continue, hence the onload.
-	//http://www.nczonline.net/blog/2009/07/28/the-best-way-to-load-external-javascript/
-	//Note: I DID NOT TEST THIS IN IE
-	if (asset.readyState){  //IE
-		asset.onreadystatechange = function(){
-		if (asset.readyState == "loaded" ||asset.readyState == "complete"){
-				asset.onreadystatechange = null;
-				return fn();
-			}
-		};
-	} else {  //Other browsers, the decent and good ones (not IE)
-		asset.addEventListener('load',fn);
-	}
+  //the Google JS API is weird and takes a while to load,
+  //so we need to be sure to load it completely before we continue, hence the onload.
+  //http://www.nczonline.net/blog/2009/07/28/the-best-way-to-load-external-javascript/
+  //Note: I DID NOT TEST THIS IN IE
+  if (asset.readyState){  //IE
+    asset.onreadystatechange = function(){
+    if (asset.readyState == "loaded" ||asset.readyState == "complete"){
+        asset.onreadystatechange = null;
+        return fn();
+      }
+    };
+  } else {  //Other browsers, the decent and good ones (not IE)
+    asset.addEventListener('load',fn);
+  }
 }
 
 var ui = {
@@ -268,7 +268,7 @@ var ui = {
     window.scroll(0,0);
 
     //TODO Implement templating system 
-   
+
     var fragment = document.createDocumentFragment();
 
     skimr_div = this.buildTag('div', this.NAMESPACE, '');
@@ -348,7 +348,7 @@ var ui = {
         yy,
         mm,
         dd;
-    
+
     fragment.appendChild(table);
     table.id = this.IDNAMESPACE + 'table';
 
@@ -369,7 +369,7 @@ var ui = {
       mm = mm.length == 1 ? "0" + mm : mm; //padding zero to month
       dd = (pub_date.getDate() + 1).toString(10);
       dd = dd.length == 1 ? "0" + dd : dd; //padding zero to day
-      
+
       table_contents += '<tr><td>' +  yy + '/' + mm + '/' + dd + '</td> ' + 
         '<td><a href="' + entry.link + '"> ' + entry.title + '</a></td></tr>\n';
     }
@@ -431,10 +431,10 @@ var ui = {
 
 //Expose skimr methods to the global namespace
 window.skimr = {
-	exitApp : exitApp,
-	init:	init,
-	pagination: ui.pagination,
-	assetReady: assetReady
+  exitApp : exitApp,
+  init: init,
+  pagination: ui.pagination,
+  assetReady: assetReady
 };
 
 //INIT
